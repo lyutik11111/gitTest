084fa8413da08203cd8b076446b071971ca06dc5
lyutik11111 <lyutik11111@gmail.com>
2022-01-10 12:00:14 +0200

    second commit

---
+++ all.js
@@ -0,0 +1,8 @@
+// let hours = +prompt('hours =');
+// let minutes = +prompt('minutes =');
+
+// let difference = 24*60 - (hours * 60 + minutes);
+// let hoursToEnd = parseInt(difference / 60);
+// let minutesToEnd = difference % 60;
+// alert(hoursToEnd);
+// alert(minutesToEnd);
